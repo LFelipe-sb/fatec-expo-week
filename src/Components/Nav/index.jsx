ebec2f8745ceb673af8bd89c6747abcba5d78c00
Luis <luisfelipe.sb@outlook.com>
2022-10-29 15:06:51 -0300

    feat(app): apply responsive layout

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -1,6 +1,6 @@
 import './style.css';
 import LogoFatec from '../../Images/logo-fatec.jpg';
-import LogoCPS from '../../Images/logo-cps.jpg';
+import LogoEtecEmbu from '../../Images/logo-etec.jpg';
 import { Link } from 'react-router-dom';
 
 function Nav() {
@@ -12,13 +12,16 @@ function Nav() {
 
   return (
     <div className="container-nav">
-      <Link className='fatec' to='/'>
-        <img className='fatec-img' src={LogoFatec} alt="Logo da Fatec" title="Logo da Fatec" />
-      </Link>
-      <img className='cps' src={LogoCPS} alt="Logo do Centro Paula Souza" title="Logo do Centro Paula Souza" />
       <Link to='/login'>
         <p className='login' onClick={sessionStorage.getItem("login") ? handleLogout : ''}>{sessionStorage.getItem("login") ? 'Sair' : 'Area Restrita'}</p>
       </Link>
+      <Link className='fatec' to='/'>
+        <img className='fatec-img' src={LogoFatec} alt="Logo da Fatec" title="Logo da Fatec" />
+      </Link>
+      <div className='apoio'> 
+        APOIO:
+        <img className='etec-embu' src={LogoEtecEmbu} alt="Logo do Centro Paula Souza" title="Logo do Centro Paula Souza" />
+      </div>
     </div>
   );
 }
